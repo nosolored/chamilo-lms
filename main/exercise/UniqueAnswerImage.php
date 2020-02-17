f97510fb7f92d372d30e1c34d8af99529a90cbf2
Nosolored SL <github@nosolored.com>
2020-02-17 11:28:16 +0100

    Applied fixes from FlintCI

--- main/exercise/UniqueAnswerImage.php
+++ main/exercise/UniqueAnswerImage.php
@@ -59,17 +59,17 @@ class UniqueAnswerImage extends UniqueAnswer
 
         $html = '<div class="alert alert-success" role="alert">'.
                 get_lang('UniqueAnswerImagePreferredSize200x150').'</div>';
-        
+
         $zoomOptions = api_get_configuration_value('zoom_images_quiz');
         if (isset($zoomOptions['options'])) {
             $finderFolder = api_get_path(WEB_PATH).'vendor/studio-42/elfinder/';
             $html .= '<!-- elFinder CSS (REQUIRED) -->';
             $html .= '<link rel="stylesheet" type="text/css" media="screen" href="'.$finderFolder.'css/elfinder.full.css">';
             $html .= '<link rel="stylesheet" type="text/css" media="screen" href="'.$finderFolder.'css/theme.css">';
-            
+
             $html .= '<!-- elFinder JS (REQUIRED) -->';
             $html .= '<script type="text/javascript" src="'.$finderFolder.'js/elfinder.full.js"></script>';
-            
+
             $html .= '<!-- elFinder translation (OPTIONAL) -->';
             $language = 'en';
             $platformLanguage = api_get_interface_language();
