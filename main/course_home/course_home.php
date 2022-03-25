4cace51f30050d1a9abf32fe5679cca6de3e7c88
Juan Cortizas Ponte <48205899+juan-cortizas-ponte@users.noreply.github.com>
2022-03-25 13:46:41 +0100

    fix course name & code

--- main/course_home/course_home.php
+++ main/course_home/course_home.php
@@ -587,8 +587,10 @@ if (api_is_student() || api_is_teacher()) {
             }
             $meeting = $pluginZoom->getMeetingRepository()->findOneBy(['meetingId' => $meetingItemId]);
             $meetingInfoGet = $meeting->getMeetingInfoGet();
+            $courseName = $meeting->getCourse();
+            $courseCode = $meeting->getCourse()->getCode(); 
             $zoomHtml .= '<tr>';
-            $zoomHtml .= '<td>'.$meeting->getMeetingInfoGet().'</td>';
+            $zoomHtml .= '<td>'.$courseName.'</td>';
             /*
             $min = $meetingInfoGet->duration > 0 ? ' ('.$meetingInfoGet->duration.' min)' : '';
             $zoomHtml .= '<td>'.$meetingInfoGet->topic.$min.'</td>';
@@ -600,7 +602,7 @@ if (api_is_student() || api_is_teacher()) {
             if (!$meeting->checkStartDateTime()) {
                 $zoomHtml .= 'No disponible';
             } else {
-                $zoomHtml .= '<a class="btn btn-primary btn-xs" href="'.$pluginPath.'zoom/join_meeting.php?meetingId='.$meetingItemId.'&cidReq='.$infoCourse['code'].'&id_session='.$meeting->getSession()->getId().'">';
+                $zoomHtml .= '<a class="btn btn-primary btn-xs" href="'.$pluginPath.'zoom/join_meeting.php?meetingId='.$meetingItemId.'&cidReq='.$courseCode.'&id_session='.$meeting->getSession()->getId().'">';
                 $zoomHtml .= $pluginZoom->get_lang('Join');
                 $zoomHtml .= '</a>';
             }
