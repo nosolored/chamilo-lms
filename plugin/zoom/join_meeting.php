dbc2ebc9fba68b95a5aa22ed31d6273c8f0ab147
Juan Cortizas Ponte <48205899+juan-cortizas-ponte@users.noreply.github.com>
2022-03-30 18:16:59 +0200

    abrir meeting de zoom en otra ventana

--- plugin/zoom/join_meeting.php
+++ plugin/zoom/join_meeting.php
@@ -44,7 +44,7 @@ try {
         $startJoinURL = null;
     }
     if (!empty($startJoinURL)) {
-        $content .= Display::url($plugin->get_lang('EnterMeeting'), $startJoinURL, ['class' => 'btn btn-primary']);
+        $content .= Display::url($plugin->get_lang('EnterMeeting'), $startJoinURL, ['target' => '_blank', 'class' => 'btn btn-primary']);
     } else {
         $content .= Display::return_message($plugin->get_lang('ConferenceNotAvailable'), 'warning');
     }
@@ -53,7 +53,7 @@ try {
         $content .= '&nbsp;'.Display::url(
             get_lang('Details'),
             api_get_path(WEB_PLUGIN_PATH).'zoom/meeting.php?meetingId='.$meeting->getMeetingId(),
-            ['target' => '_blank', 'class' => 'btn btn-default']
+            ['class' => 'btn btn-default']
         );
     }
 } catch (Exception $exception) {
